9beb8314d8d12893621f00a761bb8f535a218be8
Victoria Atkinson <105768093+VictoriPaige@users.noreply.github.com>
2022-06-23 10:13:56 -0400

    Board setup

--- script.js
+++ script.js
@@ -8,16 +8,76 @@
 
 
 
+const board = document.querySelector('#board');
+const size = 8;
+const winner = ''; //input player name option...add high point variable
 
-
-
-let grid = document.querySelector('.grid');
-for (let i = 0; i < 64; i++) {
-    const square = document.createElement('div')
-    grid.appendChild(square)
-
+function createBoard() {
+    for (let x = 0; x < size; x++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
+        for (let y = 0; y < size; y++) {
+            let tile = document.createElement('div');
+            tile.setAttribute('data-tile-num', `${x}, ${y}`);
+            row.appendChild(tile);
+        }
+        board.appendChild(row);
+    }
 }
-const squares = Array.from(document.querySelectorAll('.grid div'))
+
+function mySetPieces() {
+    let ranks = document.querySelectorAll('.row');
+
+    for (let i = 0; i < 3; i++) {
+        // if (!(i == 3 || i == 4)) {
+        for (let j = 0; j < ranks[i].children.length; j++) {
+            if (i % 2 == 0) {
+
+                if (j % 2 == 0) {
+                    ranks[i].children[j].classList.add('mypiece');
+                }
+            } else {
+                if (j % 2 != 0) {
+                    ranks[i].children[j].classList.add('mypiece');
+                }
+            }
+        }
+    }
+}
+
+const myArr = Array.from(document.querySelectorAll('.mypiece')) // <---for keeping track of which pieces are jumped? use array and pop method to keep track
+
+// FUNCTIONALITY: drag/drop eventlistener
+//can it work as is or do I need to convert into individual elements of [i]?
+
+
+
+
+
+function oppSetPieces() {
+    let ranks = document.querySelectorAll('.row');
+
+    for (let i = ranks.length - 1; i > 4; i--) {
+        for (let j = 0; j < ranks[i].children.length; j++) {
+            if (i % 2 == 0) {
+
+                if (j % 2 == 0) {
+                    ranks[i].children[j].classList.add('opppiece');
+                }
+            } else {
+                if (j % 2 != 0) {
+                    ranks[i].children[j].classList.add('opppiece');
+                }
+            }
+        }
+
+    }
+}
+const oppArr = Array.from(document.querySelectorAll('.opppiece'))
+
+createBoard();
+mySetPieces(); //add event listener (add possible moves)
+oppSetPieces(); //computer generated (add Math.random moves)
 
 
 
@@ -42,7 +102,7 @@ const squares = Array.from(document.querySelectorAll('.grid div'))
 
 //      ACTION       //
 /* Deploy event listener that allows player to select a piece and will illuminate options of where to move ONLY diagonally (either left or right)...ONE ROW AT A TIME!!!
-
+//SHOW PLAYER TURN
 
 //jumping....left and right forward
 //king me...left and right, forward and backward
